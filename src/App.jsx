3299a3e5a1c2c65019b1235ee405405c4687a9c1
ireneSimatupang <84052881+ireneSimatupang@users.noreply.github.com>
2025-05-02 20:46:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-// import './index.css';
+
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
