7845f410bd70c65ddb00f462715b9a09eb622f32
ireneSimatupang <84052881+ireneSimatupang@users.noreply.github.com>
2025-05-02 20:46:12 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
